c7d9bff38d295652d3a377f915eaf3bff5fb109f
Sha110w_4qua <49711457+shallow-zzt@users.noreply.github.com>
2024-06-19 16:47:55 +0800

    细节性更新

--- js/profile.js
+++ js/profile.js
@@ -2,55 +2,6 @@ var urlParams = new URLSearchParams(window.location.search);
 var lang = urlParams.get('lang') || 'zh-cn';
 var chara = urlParams.get('chara') || 'shallow';
 
-document.getElementById('shallow').addEventListener('click', () => {
-	chara = 'shallow';
-	setGeneric(lang);
-	setProfile(lang,chara);
-	history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);
-});
-
-document.getElementById('shellxy').addEventListener('click', () => {
-	chara = 'shellxy';
-	setGeneric(lang);
-	setProfile(lang,chara);
-	history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);
-});
-
-document.getElementById('bt').addEventListener('click', () => {
-	chara = 'bt';
-	setGeneric(lang);
-	setProfile(lang,chara);
-	history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);
-});
-
-document.getElementById('zh-cn').addEventListener('click', () => {
-	lang = 'zh-cn';
-	setGeneric(lang);
-	setProfile(lang,chara);
-	history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);
-});
-
-document.getElementById('en-us').addEventListener('click', () => {
-	lang = 'en-us';
-	setGeneric(lang);
-	setProfile(lang,chara);
-	history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);
-});
-
-document.getElementById('ja-jp').addEventListener('click', () => {
-	lang = 'ja-jp';
-	setGeneric(lang);
-	setProfile(lang,chara);
-	history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);
-});
-
-document.getElementById('ko-kr').addEventListener('click', () => {
-	lang = 'ko-kr';
-	setGeneric(lang);
-	setProfile(lang,chara);
-	history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);
-});
-
 
 // function setLanguageList(){
 // 	var filepath=`json/manifest.json`;
@@ -75,6 +26,26 @@ document.getElementById('ko-kr').addEventListener('click', () => {
 
 // }
 
+function setAvatar(){
+	var filePath=`json/manifest.json`;
+	fetch(filePath)
+    .then(response => response.json()) 
+    .then(data => {
+		charaList = data["characters"];
+		for(var i=charaList.length-1;i>=0;i--){
+			charaName = charaList[i]["name"];
+			charaAvatar = charaList[i]["avatar"];
+			if(charaAvatar != ""){
+				content = `<div class="chara_profile_avatar_select" id="${charaName}"><img src="${charaAvatar}" id="${charaName}"></div>`;
+				document.getElementById("chara_list").innerHTML += content;
+			}
+			else{
+				content = `<div class="chara_profile_avatar_select" id="${charaName}"></div>`;
+				document.getElementById("chara_list").innerHTML += content;
+			}
+		}
+    });	
+}
 
 function setGeneric(language){
 	var filePath=`json/${language}/generic.json`;
@@ -92,6 +63,9 @@ function setGeneric(language){
 			var translateId = `${translateKeys[i]}_translate`;
 			i < 8 ? document.getElementById(translateId).innerText = data[translateKeys[i]] : document.getElementById(translateId).innerText = `> ${data[translateKeys[i]]}`
 		}
+    })
+	.catch(error => {
+        console.error('Error:', error);
     });
 }
 
@@ -107,7 +81,7 @@ function setProfile(language , character){
 		}
 	  }) 
     .then(data => {
-		var avatar = data["avatar"];
+
 		var name = data["name"];
 		var birthday = data["birthday"];
 		var favorites = data["favorites"];
@@ -128,6 +102,7 @@ function setProfile(language , character){
 		document.getElementById('dislikes_content').style.display = 'block';
 		document.getElementById('details_content').style.display = 'block';
 		
+
 		name != "" ? document.getElementById('name').innerHTML = name : document.getElementById('name_content').style.display = 'none';
 		birthday != "" ? document.getElementById('birthday').innerHTML = birthday : document.getElementById('birthday_content').style.display = 'none';
 		favorites != "" ? document.getElementById('favorites').innerHTML = favorites : document.getElementById('favorites_content').style.display = 'none';
@@ -135,6 +110,7 @@ function setProfile(language , character){
 		details != "" ? document.getElementById('details').innerHTML = details : document.getElementById('details_content').style.display = 'none';
 		figure != "" ? document.getElementById('figure').innerHTML  = `<img src="${figure}" style="width: 90%; height: 100%; object-fit: cover;"><img>` : document.getElementById('figure').innerHTML  = '';
 
+		
 		document.getElementById('socials_content').style.display = 'block';
 		if(socials.length == 0){document.getElementById('socials_content').style.display = 'none';}
 		else{
@@ -167,6 +143,8 @@ function setProfile(language , character){
 				});
 			}
 		}
+
+		document.getElementById('side_project_content').style.removeProperty('column-count');
 		document.getElementById('side_project_content').style.display = 'block';
 		if(threeside.length == 0 && otherpic.length == 0 && projectSong.length == 0 && projcetProduction.length == 0){
 			document.getElementById('side_project_content').style.display = 'none';
@@ -216,13 +194,13 @@ function setProfile(language , character){
 				var author = projectSong[i]['author'];
 				var song = projectSong[i]['song'];
 				
-				pic != "" ? pic = `<div class="song_pic_area"><img src="${pic}"></div>` : pic = "";
-				author != "" ? author = `<div class="song_author">Made By </br>${author}</div>` : author = "";
+				pic != "" ? pic = `<div class="song_pic_area" id="song_author"><img src="${pic}"></div>` : pic = "";
+				author != "" ? author = `<div class="song_author id="song_author">Made By </br>${author}</div>` : author = "";
 				
 				var content = `<div class="song_frame_area" id="${title}">
 								${pic}
 								<div class="song_detail_area">
-								<div class="song_title_area_frame">
+								<div class="song_title_area_frame${author != "" ? "":"_no_author"}">
 									<div class="song_name_title">${title}</div>
 									${author}
 								</div>
@@ -274,9 +252,66 @@ function setProfile(language , character){
 		if(applyProfile == 1){
 			document.getElementById('side_project_content').style.columnCount = 1;
 		}
+    })
+	.catch(error => {
+        console.error('Error:', error);
     });
 }
 
 
 setGeneric(lang);
 setProfile(lang,chara);
+setAvatar();
+
+document.getElementById("chara_list").addEventListener('click', (event) => {
+		if(event.target.matches("#shallow")){
+			chara = 'shallow';
+			setGeneric(lang);
+			setProfile(lang,chara);
+			history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);			
+		}
+		else if(event.target.matches("#shellxy")){
+			chara = 'shellxy';
+			setGeneric(lang);
+			setProfile(lang,chara);
+			history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);			
+		}
+		else if(event.target.matches("#bt")){
+			chara = 'bt';
+			setGeneric(lang);
+			setProfile(lang,chara);
+			history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);			
+		}
+    }
+);
+
+document.getElementById("language_list").addEventListener('click', (event) => {
+	if(event.target.matches("#zh-cn")){
+		lang = 'zh-cn';
+		setGeneric(lang);
+		setProfile(lang,chara);
+		history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);			
+	}
+	else if(event.target.matches("#en-us")){
+		lang = 'en-us';
+		setGeneric(lang);
+		setProfile(lang,chara);
+		history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);		
+	}
+	else if(event.target.matches("#ja-jp")){
+		lang = 'ja-jp';
+		setGeneric(lang);
+		setProfile(lang,chara);
+		history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);
+	}
+	else if(event.target.matches("#ko-kr")){
+		lang = 'ko-kr';
+		setGeneric(lang);
+		setProfile(lang,chara);
+		history.pushState({}, '', `index.html?lang=${lang}&chara=${chara}`);	
+	}
+});
+
+
+
+
